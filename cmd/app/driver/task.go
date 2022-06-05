c53ee6cdd3aa2b29efc334bfc8422bc8a9c432f5
yuya <cheeeese4869@gmail.com>
2022-06-05 15:31:41 +0900

    add driver

--- cmd/app/driver/task.go
+++ cmd/app/driver/task.go
@@ -1,3 +1,40 @@
 package drivers
 
-type
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/lib/pq"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/yuya0729/go-clean-architecture/tree/develop/cmd/app/adapter/controller"
+	"github.com/yuya0729/go-clean-architecture/tree/develop/cmd/app/adapter/geteway"
+	"github.com/yuya0729/go-clean-architecture/tree/develop/cmd/app/adapter/presenter"
+	"github.com/yuya0729/go-clean-architecture/tree/develop/cmd/app/usecase/interactor"
+)
+
+func Serve() {
+	dsn := fmt.Sprint("user=postgres host=postgres port=5432 dbname=postgres password=postgres sslmode=disable")
+	conn, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	task := controller.Task{
+		OutputFactory: presenter.NewTaskOutputPort,
+		InputFactory:  interactor.NewTaskInputPort,
+		RepoFactory:   geteway.NewTaskRepository,
+		Conn:          conn,
+	}
+
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/api/v1/tasks", task.GetTasks)
+	e.Start(":8080")
+}
